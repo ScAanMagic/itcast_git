94880127c12066454236c392e11f0921a2db8a7a
magic <870418829@qq.com>
2019-01-04 15:41:50 +0800

    新建User.java

---
+++ src/main/java/com/itheima/domain/user.java
@@ -0,0 +1,13 @@
+package com.itheima.domain;
+
+/**
+ * @program: springData-Boot
+ * @description:
+ * @author: Magic冲
+ * @create: 2019-01-04 15:35
+ **/
+public class user {
+    private Integer id;
+    private String name;
+
+}
